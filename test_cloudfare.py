d0557ef9961bacefb455e6ce7b543926bf3fdb18
beidi <beidi.beer@gmail.com>
2025-03-27 12:06:06 +0800

    testing cdp mode

--- test_cloudfare.py
+++ test_cloudfare.py
@@ -2,7 +2,7 @@ from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
     url = "https://gitlab.com/users/sign_in"
-    sb.uc_open_with_reconnect(url, 5)
+    sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
     sb.sleep(2)
     sb.uc_gui_handle_captcha()
