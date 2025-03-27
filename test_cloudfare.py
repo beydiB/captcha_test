21b1c1bcc6afdd748a3eeb4eb08b963a53cad007
beidi <beidi.beer@gmail.com>
2025-03-27 12:52:50 +0800

    test proxy

--- test_cloudfare.py
+++ test_cloudfare.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, proxy="boss:Money!@222.127.189.131:8080") as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
