56dad92ab1ecefe6af3509289d9145846725d652
beidi <beidi.beer@gmail.com>
2025-03-27 11:51:48 +0800

    test

--- test_cloudflare.py
+++ test_cloudflare.py
@@ -6,4 +6,9 @@ with SB(uc=True, test=True) as sb:
     sb.uc_gui_click_captcha()
     sb.sleep(2)
     sb.uc_gui_handle_captcha()
-   
+    sb.assert_text("Username", '[for="user_login"]', timeout=3)
+    sb.assert_element('label[for="user_login"]')
+    sb.highlight('button:contains("Sign in")')
+    sb.highlight('h1:contains("GitLab.com")')
+    sb.post_message("SeleniumBase wasn't detected", duration=4)
+    print("Success! Website did not detect SeleniumBase!")
