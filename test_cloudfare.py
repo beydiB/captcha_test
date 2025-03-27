8fd1ec98d13b7eb5f4ecc541af479d93f35f8153
beidi <beidi.beer@gmail.com>
2025-03-27 11:55:56 +0800

    test

--- test_cloudfare.py
+++ test_cloudfare.py
@@ -1,9 +1,14 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
-    url = "https://seleniumbase.io/apps/turnstile"
-    sb.uc_open_with_reconnect(url, 4)
+    url = "https://gitlab.com/users/sign_in"
+    sb.uc_open_with_reconnect(url, 5)
     sb.uc_gui_click_captcha()
-    sb.assert_element("img#captcha-success", timeout=3)
-    sb.set_messenger_theme(location="top_left")
-    sb.post_message("SeleniumBase wasn't detected", duration=3)
+    sb.sleep(2)
+    sb.uc_gui_handle_captcha()
+    sb.assert_text("Username", '[for="user_login"]', timeout=3)
+    sb.assert_element('label[for="user_login"]')
+    sb.highlight('button:contains("Sign in")')
+    sb.highlight('h1:contains("GitLab.com")')
+    sb.post_message("SeleniumBase wasn't detected", duration=4)
+    print("Success! Website did not detect SeleniumBase!")
