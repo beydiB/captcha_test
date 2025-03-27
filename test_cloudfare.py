b480d41d281745712252d7e81e37585696376489
beidi <beidi.beer@gmail.com>
2025-03-28 07:49:01 +0800

    added github secret

--- test_cloudfare.py
+++ test_cloudfare.py
@@ -1,9 +1,12 @@
+import os
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, proxy=os.getenv("PROXY")) as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
     sb.sleep(2)
     sb.uc_gui_handle_captcha()
-   
+    sb.sleep(20)
+
+    #leveae window open
