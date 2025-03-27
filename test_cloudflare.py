3661fa08de5ce2ec2a53b0aac91ef5505344c56c
beidi <beidi.beer@gmail.com>
2025-03-27 11:35:50 +0800

    changed script

--- test_cloudflare.py
+++ test_cloudflare.py
@@ -1,6 +1,11 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
-    url = "https://seleniumbase.io/apps/turnstile"
+    url = "https://www.virtualmanager.com/en/login"
     sb.uc_open_with_reconnect(url, 4)
     sb.uc_gui_click_captcha()
+    print(sb.get_page_title())
+    sb.assert_element('input[name*="email"]')
+    sb.assert_element('input[name*="login"]')
+    sb.set_messenger_theme(location="bottom_center")
+    sb.post_message("SeleniumBase wasn't detected!")
