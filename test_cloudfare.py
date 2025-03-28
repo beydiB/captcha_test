2b3356c140d67d266f9a8b5314fdf21d0b9a2bc2
beidi <beidi.beer@gmail.com>
2025-03-28 10:12:39 +0800

    fixed click_captcha

--- test_cloudfare.py
+++ test_cloudfare.py
@@ -1,12 +1,11 @@
 import os
 from seleniumbase import SB
 
-
 with SB(uc=True, test=True, proxy=os.environ["PROXY"]) as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
     sb.sleep(2)
-    sb.uc_gui_handle_captcha()
-    sb.sleep(20)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2)
 
