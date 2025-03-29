3b0c6cfc38c38b999657bb155d85b3c1e69feb02
beidi <beidi.beer@gmail.com>
2025-03-29 18:23:42 +0800

    testing setting

--- go.py
+++ go.py
@@ -15,13 +15,7 @@ def main():
     url = os.environ['UPWORK_SEARCH_URL']
     
     with SB(uc=True, test=True, proxy=os.environ["PROXY"], locale="en") as sb:
-        sb.activate_cdp_mode(url)
-        sb.uc_gui_click_captcha()
-        sb.sleep(2)
-        sb.uc_gui_click_captcha()
-        sb.sleep(2)
-        sb.uc_gui_click_captcha()
-        sb.sleep(2)
+        sb.uc_open_with_reconnect(url, 8)
         sb.uc_gui_click_captcha()
         sb.sleep(2)
 
