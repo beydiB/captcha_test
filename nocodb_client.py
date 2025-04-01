c4011b4eb582820fce3fd1b3d628273950683dce
beidi <beidi.beer@gmail.com>
2025-04-01 10:04:32 +0800

    added posted_date for deleting reows

--- nocodb_client.py
+++ nocodb_client.py
@@ -49,7 +49,7 @@ class NocodbClient:
                 delete_params = {
                     "limit": 25,
                     "sort": "job_uid",  # Sort ascending to get oldest records
-                    "fields": "Id,job_uid",  # Only get id and job_uid fields
+                    "fields": "Id,post_date",  # Only get id and job_uid fields
                     # "select": "id"  # Explicitly select only these fields
                 }
                 response = requests.get(self.base_url, headers=self.headers, params=delete_params)
